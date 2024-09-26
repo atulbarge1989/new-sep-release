897aff660d078a0dcd3e6864021b970cf0bcebfe
atulbarge1989 <149707677+atulbarge1989@users.noreply.github.com>
2024-09-27 01:11:23 +0530

    Add files via upload

---
+++ project_details.sql
@@ -0,0 +1,4 @@
+INSERT into project_details (aspice,iso,project_id,sopdate,domain_categories,oem,project_code,project_desc,project_leader,project_name,project_status,project_members) VALUES 
+(true,true,1,'2026-08-31 05:30:00','EV','BMW','PFS-24-001','Created new project for mobis','HONG GIL DONG','MEB','In-Progress','{Leen}'), 		
+(true,true,2,'2024-07-16 05:30:00','EV','VW','PFS-24-002','Created new project for mobis','LEE SUN HYUNG','ICCU-GEN1','Completed','{Leen,"Mr. Yun"}'), 		
+(true,true,3,'2028-11-30 05:30:00','EC','STELANTIS','PFS-24-003','New project added','KIM KYUNG MAN', 		'BDC2','Created','{"Mr. Park"}');
