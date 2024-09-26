7a58fb87924187dae100b615aa2a537596beb791
atulbarge1989 <149707677+atulbarge1989@users.noreply.github.com>
2024-09-27 01:11:43 +0530

    Add files via upload

---
+++ polyspace_bug_finder_report.sql
@@ -0,0 +1,18 @@
+
+INSERT into polyspace_bug_finder_report (project_id,report_id,category,count,reviewed,status,unreviewed) VALUES
+									(1,1,'Polyspace Code Metrics',30690,0,'NA',30690),
+									(1,2,'CERT C Coding Standard',331,0,'NA',331),
+									(1,3,'Common Weakness Enumeration (CWE)',122,0,'NA',122),
+									(1,4,'Defects',0,0,'NA',0);
+									
+INSERT into polyspace_bug_finder_report (project_id,report_id,category,count,reviewed,status,unreviewed) VALUES
+								(2,5,'Polyspace Code Metrics',30690,17278,'NA',13412),
+								(2,6,'CERT C Coding Standard',331,0,'NA',331),
+								(2,7,'Common Weakness Enumeration (CWE)',122,122,'NA',0),
+								(2,8,'Defects',0,0,'NA',0);
+  
+INSERT into polyspace_bug_finder_report (project_id,report_id,category,count,reviewed,status,unreviewed) VALUES
+								(3,9,'Polyspace Code Metrics',30690,9716,'NA',20974),
+								(3,10,'CERT C Coding Standard',331,331,'NA',0),
+								(3,11,'Common Weakness Enumeration (CWE)',122,122,'NA',0),
+								(3,12,'Defects',0,0,'NA',0);
